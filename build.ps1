1cd627f7787f646d0ff4862cd2ddef5968fb6b9d
George G <grreulvi@gmail.com>
2026-04-12 01:52:55 +0300

    add -run and -clean flags to build.ps1

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-param([switch]$configure)
+param([switch]$configure, [switch]$clean, [switch]$run)
 # dependencies:
 # lua 5.3.5 bulid with msvc 15 x64
 # SDL2-2.0.22       with dynamic linking
@@ -8,19 +8,29 @@ param([switch]$configure)
 # sol2 ver. 3.2+
 # glm 0.9.9.4 (https://glm.g-truc.net)
 # imgui 1.79 (https://github.com/ocornut/imgui)
-$sysEnv = ConvertFrom-Json $(.\get-env.ps1)
-$ninjaPath = $sysEnv.NinjaBuildPath
 
-$cmakeCall = @('cmake', '-S', '.', '-B', './build', '-G', 'Ninja', 
-          '-DCMAKE_EXPORT_COMPILE_COMMANDS=1', 
-          '-DCMAKE_C_COMPILER=clang',
-          '-DCMAKE_CXX_COMPILER=clang++',
-          "-DCMAKE_MAKE_PROGRAM=$($sysEnv.NinjaBuildPath)")
+$sysEnv = ConvertFrom-Json $(.\get-env.ps1)
 
 if ($configure) {
+  $cmakeCall = @('cmake', '-S', '.', '-B', './build', '-G', 'Ninja', 
+            '-DCMAKE_EXPORT_COMPILE_COMMANDS=1', 
+            '-DCMAKE_C_COMPILER=clang',
+            '-DCMAKE_CXX_COMPILER=clang++',
+            "-DCMAKE_MAKE_PROGRAM=$($sysEnv.NinjaBuildPath)")
+
   New-Item -ItemType Directory -Name 'build' -Force | Out-Null
   Invoke-Expression $($cmakeCall -join ' ')
-  #cmake -S . -B ./build -G "Ninja" -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_MAKE_PROGRAM="$($sysEnv.NinjaBuildPath)"
+}
+elseif ($clean) {
+  Remove-Item -recurse -force ./build -ErrorAction SilentlyContinue
+  Remove-Item ./build_commands.json -ErrorAction SilentlyContinue
+}
+elseif ($run) {
+  Copy-Item "$($sysEnv.Sdl2.Core)\lib\x64\SDL2.dll" .\build\ -ErrorAction SilentlyContinue
+  Copy-Item "$($sysEnv.Sdl2.Image)\lib\x64\SDL2_image.dll" .\build\ -ErrorAction SilentlyContinue
+  Copy-Item "$($sysEnv.Sdl2.Mixer)\lib\x64\SDL2_mixer.dll" .\build\ -ErrorAction SilentlyContinue
+  Copy-Item "$($sysEnv.Sdl2.Ttf)\lib\x64\SDL2_ttf.dll" .\build\ -ErrorAction SilentlyContinue
+  ./build/engine.exe
 }
 else {
   cmake --build ./build
